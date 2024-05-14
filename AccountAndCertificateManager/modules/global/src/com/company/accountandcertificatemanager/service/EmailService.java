3358dc5554e62f3467bb5436e5691d3995184ad6
Alexey-Fog <alpetrov.2003@gmail.com>
2024-05-14 14:12:36 +0300

    new project... again...

--- AccountAndCertificateManager/modules/global/src/com/company/accountandcertificatemanager/service/EmailService.java
+++
@@ -1,7 +0,0 @@
-package com.company.accountandcertificatemanager.service;
-
-public interface EmailService {
-    String NAME = "accountandcertificatemanager_EmailService";
-
-    void sendCertificatesByEmail(String userEmail, String[] certificateFiles);
-}
